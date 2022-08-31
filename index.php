859af442463bc2ba008f5fca54ac12272403239a
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-08-31 18:25:40 +0200

    added-some-css

--- index.php
+++ index.php
@@ -72,11 +72,12 @@ $faqs_array = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
     
-    <main>
+    <main class = "container" >
           <?php foreach ($faqs_array as $faqs) { ?>
                 <section>
                     <?php foreach($faqs as $faq) {?>
